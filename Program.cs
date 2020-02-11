86cc231462c8aa53352ba0015d8193acecc2a3e7
Mario <mspformacion1020@gmail.com>
2020-02-11 11:59:49 +0100

    Creando clases e interfaces

--- Program.cs
+++ Program.cs
@@ -7,6 +7,86 @@ namespace interfazNew
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ICharlatan loro = new Loro("Ave","Voladora");
+                        
+            loro.hablar();
+
+
         }
     }
+
+
+    interface ICharlatan{
+
+        public void hablar();
+
+    }
+
+    abstract class Animal : ICharlatan{
+
+        private string especie;
+
+        public Animal(string especie){
+
+            this.especie = especie;
+
+        }
+
+        public virtual void hablar(){          
+
+            Console.WriteLine("Hablamos");
+
+        }
+
+    }
+
+    class Loro : Animal{
+
+        private string tipo;
+
+        public Loro(string especie,string tipo) : base(especie){
+
+            this.tipo = tipo;
+
+        }
+
+        public override void hablar(){
+
+            //base.hablar();
+            Console.WriteLine("Muy poco");
+
+
+        }
+
+
+    }
+
+    class Delfin : Animal{
+        private string tipo;
+
+        public Delfin(string especie,string tipo) : base(especie){
+
+            this.tipo = tipo;
+
+        }
+
+
+    }
+
+    class Tortuga : Animal{
+
+        private string tipo;
+
+        public Tortuga(string especie,string tipo) : base(especie){
+
+            this.tipo = tipo;
+
+        }
+
+    }
+
+
+
+
 }
